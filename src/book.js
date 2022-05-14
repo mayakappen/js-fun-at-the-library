a6da80fb98295f3258ce90b9ab39667273006824
Maya Kappen <mayakappen@gmail.com>
2022-05-14 15:08:39 -0600

    stuck on editBook

--- src/book.js
+++ src/book.js
@@ -1,12 +1,52 @@
-function createTitle() {
-  
+function createTitle(title) {
+
+  return "The " + title;
+
+};
+
+function buildMainCharacter(name, age, pronouns) {
+return {
+  name: name,
+  age: age,
+  pronouns: pronouns
 }
+};
+
+function saveReview(review, reviews) {
+if (reviews.indexOf(review) === -1) {
+reviews.push(review);
+}
+}
+
+function calculatePageCount(bookTitle, bookPageCount) {
+bookPageCount = bookTitle.length * 20;
+return bookPageCount;
+}
+
+function writeBook(title, character, genre) {
+var book = {
+    title: title,
+    mainCharacter: character,
+    pageCount: calculatePageCount(title),
+    genre: genre
+
+  }
+  return book
+}
+
+function editBook(title) {
+var newTitle = 3/4 * calculatePageCount(title);
+return newTitle
+}
+
+
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
