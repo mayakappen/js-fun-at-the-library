8b1030a7a2a70ea3a092de1e45aef8e65b1b80b5
Maya Kappen <mayakappen@gmail.com>
2022-05-15 20:06:31 -0600

    add createLibrary

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,24 @@
+function createLibrary(library) {
+var shelves = {
+  fantasy: [],
+  fiction: [],
+  nonFiction: []
+}
+  var library = {
+    name: library,
+    shelves: shelves
+  }
+return library
+}
 
+function addBook(library, book) {
+  if book.genre = "fantasy" {
+book.push(library.shelves.fantasy);
+  }
+  return book
+}
 
-module.exports = {
-  // createLibrary,
-  // addBook,
+createLibrary,
+addBook,
   // checkoutBook
 };
