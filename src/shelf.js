913b6a25662f253eda2d1ba5ce4293923ab676ce
Maya Kappen <mayakappen@gmail.com>
2022-05-15 18:16:43 -0600

    add shelf

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,40 @@
+function shelfBook(title, shelf) {
+if (shelf.length < 3) {
+return shelf.unshift(title);
+}
+};
+
+function unshelfBook(title, shelf) {
+  for ( var i = 0; i < shelf.length; i++)
+  {
+    if (shelf[i].title === title) {
+
+    return shelf.splice(i, 1);
+  }
+}
+};
+
+function listTitles(book) {
+  var titles = []
+  for (var i = 0; i < 3; i++) {
+titles.push(book[i].title);
+}
+return titles.join(', ');
+};
+
+function searchShelf(shelf, book) {
+var onShelf = false
+  for (var i = 0; i < shelf.length; i++) {
+   if (shelf[i].title === book) {
+     onShelf = true
+   }
+}
+return onShelf
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
-};
+shelfBook,
+unshelfBook,
+listTitles,
+searchShelf
+}
